4192242ab1c8400aa05279445a57ca929358cfa9
koyamal <74910401+koyamal@users.noreply.github.com>
2022-10-01 10:23:23 +0900

    Practice2: Type of Output from function

---
+++ src/practices/Practice2.tsx
@@ -0,0 +1,15 @@
+export const Practice2 = () => {
+  const getTotalFee = (num: number): number => {
+    const total = num * 1.1;
+    return total;
+  };
+  const onClickPractice = () => {
+    console.log(getTotalFee(1000));
+  };
+  return (
+    <div>
+      <p>Practice2:Type of Output from function</p>
+      <button onClick={onClickPractice}>Practice1 start</button>
+    </div>
+  );
+};
